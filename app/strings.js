146571e391168fddf4cb2a7e59d67a3f9afb8498
Yerson Copa <yerson.copa@jalasoft.com>
2021-12-01 10:58:44 -0400

    Solved the tasks using javascript

--- app/strings.js
+++ app/strings.js
@@ -1,27 +1,41 @@
 stringsAnswers = {
   /**
    * Reduces a string by removing letters that repeat more than amount times.
-   * 
+   *
    * Example: reduceString('aaaabbb', 1) should reduce to 'ab'
    * Example: reduceString('aaaabbb', 2) should reduce to 'aabb'
-   * 
+   *
    * @param {String} str - String that is to be reduced
    * @param {Number} amount - The maximum number of adjacent repeated letters in the result string.
    * @returns {String} A string with no more than amount number of repeated letters.
    */
   reduceString: function reduceString(str, amount) {
-
+    const strArr = str.split('');
+    let amounCopy = amount;
+    let result = '';
+    for (let i = 0; i < strArr.length; i += 1) {
+      if (strArr[i] === strArr[i + 1]) {
+        amounCopy -= 1;
+        if (amounCopy > 0) {
+          result += strArr[i];
+        }
+      } else {
+        amounCopy = amount;
+        result += str[i];
+      }
+    }
+    return result;
   },
 
   /**
    * Reverses a string of text
-   * 
+   *
    * Example: reverseString('abc') should be 'cba'
-   * 
+   *
    * @param {String} str - a string of text to be reversed
    * @returns {String} The original string of text str reversed.
    */
   reverseString: function reverseString(str) {
-
+    return str.split('').reverse().join('');
   },
 };
